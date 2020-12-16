a127024d91d67a0ab445495dcd91f556d05a3ae3
javiermontanov <javiermontanov@gmail.com>
2020-12-16 10:45:11 -0400

    Adding changes for username

--- support-api.services/Extensions/ApplicationBuilderExtensions.cs
+++ support-api.services/Extensions/ApplicationBuilderExtensions.cs
@@ -63,13 +63,13 @@ namespace support_api.services.Extensions
 			if (!context.SupportApiUser.Any())
 			{
 				supportApiUser.Organization = organization;
-				supportApiUser.Username = "JavierUserApi";
-				supportApiUser.Password = "JavierUserApi";
+				supportApiUser.Username = "Capy1";
+				supportApiUser.Password = "Capy1";
 				supportApiUser.SupportApiUser_UserKobo = new List<SupportApiUser_UserKobo>();
 
 				supportApiUser2.Organization = organization;
-				supportApiUser2.Username = "MontanovUserApi";
-				supportApiUser2.Password = "MontanovUserApi";
+				supportApiUser2.Username = "Capy2";
+				supportApiUser2.Password = "Capy2";
 				supportApiUser2.SupportApiUser_UserKobo = new List<SupportApiUser_UserKobo>();
 
 				context.SupportApiUser.Add(supportApiUser);
