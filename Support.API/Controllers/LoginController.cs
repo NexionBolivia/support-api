81c8ac7173883f759cc7c3acd9488383bf460002
javiermontanov <javiermontanov@gmail.com>
2020-12-22 16:05:26 -0400

    Adding changes for services and controllers

--- Support.API/Controllers/LoginController.cs
+++ Support.API/Controllers/LoginController.cs
@@ -1,11 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SupportAPI.Services.Services;
+using Newtonsoft.Json.Linq;
+using Support.API.Services.Data;
+using Support.API.Services.Models;
 
-namespace Support.Api.Controllers
+namespace support_api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class LoginController : ControllerBase
     {
+        private readonly ApplicationDbContext context;
+        private readonly IProfileService profileService;
 
+
+        public LoginController(ApplicationDbContext context)
+        {
+            this.context = context;
+            this.profileService = new ProfileService(context);
+        }
+
+        [HttpPost]
+        public LoginResponse Authenticate(JObject? data)
+        {
+            return this.profileService.Login(data);
+        }
     }
 }
--- Support.API/Controllers/ProfileController.cs
+++ Support.API/Controllers/ProfileController.cs
@@ -1,11 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SupportAPI.Services.Services;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Http;
+using Support.API.Services.Data;
+using Support.API.Services.Models;
 
-namespace Support.Api.Controllers
+namespace support_api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class ProfileController : ControllerBase
     {
-        
+
+        private readonly ApplicationDbContext context;
+        private readonly IProfileService profileService;
+
+
+        public ProfileController(ApplicationDbContext context)
+        {
+            this.context = context;
+            this.profileService = new ProfileService(context);
+        }
+
+        [HttpGet]
+        public IEnumerable<CompositeProfile> GetProfiles(JObject? data)
+        {
+            return this.profileService.GetProfiles(data);
+        }
+
+        [HttpPost]
+        public ActionResult CreateProfile(JObject? data)
+        {
+            if (this.profileService.CreateProfile(data)) return Ok();
+            else return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        [HttpPut]
+        public ActionResult UpdateProfile(string username, JObject? data)
+        {
+            if (this.profileService.UpdateProfile(data)) return Ok();
+            else return StatusCode(StatusCodes.Status500InternalServerError);
+        }
     }
 }
