9cbd7eb6243e1e4fc7c8c4f06a63fe360a0e43aa
Samuel Rioja <samuelriotz@hotmail.com>
2021-03-15 10:27:34 -0400

    local changes

--- Support.API.Services/Helpers/LocalDb.cs
+++ Support.API.Services/Helpers/LocalDb.cs
@@ -4,20 +4,20 @@ public static class LocalDb
 {
     public static Dictionary<string, string> Values = new Dictionary<string, string>()
     {
-        { "SUPPORT_DB_SERVER", "localhost" },
-        { "SUPPORT_DB_INTERNAL_SERVER", "localhost" },
-        { "SUPPORT_DB_PORT", "5440" },
-        { "SUPPORT_DB_INTERNAL_PORT", "5440" },
+        { "SUPPORT_DB_SERVER", "192.168.100.3" },
+        { "SUPPORT_DB_INTERNAL_SERVER", "192.168.100.3" },
+        { "SUPPORT_DB_PORT", "5432" },
+        { "SUPPORT_DB_INTERNAL_PORT", "5432" },
         { "SUPPORT_DB_NAME", "support" },
         { "SUPPORT_DB_USER", "user" },
         { "SUPPORT_DB_PASSWORD", "password" },
 
         { "KOBO_DB_SERVER", "192.168.100.3" },
-        { "KOBO_DB_PORT", "5432"},
+        { "KOBO_DB_PORT", "5435"},
         { "KOBO_DB_NAME", "koboform"},
         { "KOBO_DB_USER", "kobo"},
-        { "KOBO_DB_PASSWORD", "bm@uuNVJ57')"},
+        { "KOBO_DB_PASSWORD", "+)z`G#1NQYiZ"},
 
-        { "KOBO_API_SERVER", "http://kf.kobo.local" }
+        { "KOBO_API_SERVER", "http://kf.kobo.local/" }
     };
 }
